b9a47b6f40d512896ce09ef5ef63967be414fb39
Andrey Matveev <andrewmat2000@gmail.com>
2024-09-16 17:08:03 +0500

    removed sync lib

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // main.js
 
 // https://www.electronforge.io/config/makers/squirrel.windows
+
+
 if (require('electron-squirrel-startup')) return;
 
 const net = require("net")
@@ -19,143 +21,145 @@ function checkPort(port) {
     })
 }
 
-let port = 8080;
+async function getPort() {
+    let port = 8080;
 
-const sp = require('synchronized-promise')
+    do {
+        console.log(`Checking '${port}' port.`);
 
-do {
-    const fn = sp(() => checkPort(port));
+        if (await checkPort(port)) {
+            console.log(`Port '${port}' is available.`);
 
-    console.log(`Checking '${port}' port.`);
+            break;
+        } else {
 
-    if (fn()) {
-        console.log(`Port '${port}' is available.`);
+            console.log(`Port '${port}' is not available, checking next.`);
+        }
 
-        break;
-    } else {
+    } while (port++ < 65535);
 
-        console.log(`Port '${port}' is not available, checking next.`);
-    }
-
-} while (port++ < 65535);
-
-
-process.env.DEMERGI_ADDRS = `[::]:${port}`;
-
-require("./demergi.js")
-
-const { app, shell, session, BrowserWindow, Menu, Tray, nativeImage, dialog } = require('electron')
-const { getHA, setHA } = require('./settings.js');
-
-console.log(`Setting proxy to ${port}.`);
-app.commandLine.appendSwitch("proxy-server", `http://localhost:${port}`);
-
-// Disable Hardware Acceleration
-// https://www.electronjs.org/docs/latest/tutorial/offscreen-rendering
-if (!getHA()) {
-    app.disableHardwareAcceleration()
+    return port;
 }
 
-createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        title: 'YouTube Dpi Free',
-        icon: __dirname + '/images/YouTube.ico',
-        autoHideMenuBar: true,
-        webPreferences: {
-            webSecurity: true,
-            contextIsolation: true,
-            webviewTag: true,
-            nodeIntegration: true,
-            nativeWindowOpen: true
-        }
-    });
 
-    win.loadURL(`https://www.youtube.com/feed/subscriptions`);
+getPort().then((port) => {
+    process.env.DEMERGI_ADDRS = `[::]:${port}`;
 
-    // Create a Cookie, so that Theater Mode is allways enabled.
-    // https://www.electronjs.org/docs/latest/api/cookies
-    // http://blog.ercanopak.com/how-to-make-theater-mode-the-default-for-youtube/
-    // https://medium.com/swlh/building-an-application-with-electron-js-part-2-e62c23e4eb69
-    const cookie = { url: 'https://www.youtube.com', name: 'wide', value: '1' }
-    session.defaultSession.cookies.set(cookie)
-        .then(() => {
-            // success
-        }, (error) => {
-            console.error(error)
-        })
+    require("./demergi.js")
 
-    // Open links with External Browser
-    // https://stackoverflow.com/a/67409223
-    win.webContents.setWindowOpenHandler(({ url }) => {
-        shell.openExternal(url);
-        return { action: 'deny' };
-    });
+    const { app, shell, session, BrowserWindow, Menu, Tray, nativeImage, dialog } = require('electron')
+    const { getHA, setHA } = require('./settings.js');
 
-    let tray = null
-    const icon = nativeImage.createFromPath(__dirname + '/images/YouTube.ico')
-    tray = new Tray(icon)
+    console.log(`Setting proxy to ${port}.`);
+    app.commandLine.appendSwitch("proxy-server", `http://localhost:${port}`);
 
-    const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Hardware Acceleration',
-            type: 'checkbox',
-            checked: getHA(),
-            click({ checked }) {
-                setHA(checked)
-                dialog.showMessageBox(
-                    null,
-                    {
-                        type: 'info',
-                        title: 'info',
-                        message: 'Exiting Applicatiom, as Hardware Acceleration setting has been changed...'
-                    })
-                    .then(result => {
-                        if (result.response === 0) {
-                            app.relaunch();
-                            app.exit()
+    // Disable Hardware Acceleration
+    // https://www.electronjs.org/docs/latest/tutorial/offscreen-rendering
+    if (!getHA()) {
+        app.disableHardwareAcceleration()
+    }
+
+    createWindow = () => {
+        const win = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            title: 'YouTube Dpi Free',
+            icon: __dirname + '/images/YouTube.ico',
+            autoHideMenuBar: true,
+            webPreferences: {
+                webSecurity: true,
+                contextIsolation: true,
+                webviewTag: true,
+                nodeIntegration: true,
+                nativeWindowOpen: true
+            }
+        });
+
+        win.loadURL(`https://www.youtube.com`);
+
+        // Create a Cookie, so that Theater Mode is allways enabled.
+        // https://www.electronjs.org/docs/latest/api/cookies
+        // http://blog.ercanopak.com/how-to-make-theater-mode-the-default-for-youtube/
+        // https://medium.com/swlh/building-an-application-with-electron-js-part-2-e62c23e4eb69
+        const cookie = { url: 'https://www.youtube.com', name: 'wide', value: '1' }
+        session.defaultSession.cookies.set(cookie)
+            .then(() => {
+                // success
+            }, (error) => {
+                console.error(error)
+            })
+
+        // Open links with External Browser
+        // https://stackoverflow.com/a/67409223
+        win.webContents.setWindowOpenHandler(({ url }) => {
+            shell.openExternal(url);
+            return { action: 'deny' };
+        });
+
+        let tray = null
+        const icon = nativeImage.createFromPath(__dirname + '/images/YouTube.ico')
+        tray = new Tray(icon)
+
+        const contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Hardware Acceleration',
+                type: 'checkbox',
+                checked: getHA(),
+                click({ checked }) {
+                    setHA(checked)
+                    dialog.showMessageBox(
+                        null,
+                        {
+                            type: 'info',
+                            title: 'info',
+                            message: 'Exiting Applicatiom, as Hardware Acceleration setting has been changed...'
+                        })
+                        .then(result => {
+                            if (result.response === 0) {
+                                app.relaunch();
+                                app.exit()
+                            }
                         }
-                    }
-                    )
+                        )
+                }
+            },
+            {
+                label: 'Clear Cache',
+                click: () => {
+                    session.defaultSession.clearStorageData()
+                    app.relaunch();
+                    app.exit();
+                }
+            },
+            {
+                label: 'Reload',
+                click: () => win.reload()
+            },
+            {
+                label: 'Quit',
+                type: 'normal',
+                role: 'quit'
             }
-        },
-        {
-            label: 'Clear Cache',
-            click: () => {
-                session.defaultSession.clearStorageData()
-                app.relaunch();
-                app.exit();
+        ])
+
+        tray.setToolTip('YouTube Dpi Free')
+        tray.setTitle('YouTube Dpi Free')
+        tray.setContextMenu(contextMenu)
+    };
+
+    app.whenReady().then(() => {
+        createWindow()
+
+        app.on('activate', () => {
+            if (BrowserWindow.getAllWindows().length === 0) {
+                createWindow()
             }
-        },
-        {
-            label: 'Reload',
-            click: () => win.reload()
-        },
-        {
-            label: 'Quit',
-            type: 'normal',
-            role: 'quit'
-        }
-    ])
+        })
+    })
 
-    tray.setToolTip('YouTube Dpi Free')
-    tray.setTitle('YouTube Dpi Free')
-    tray.setContextMenu(contextMenu)
-};
-
-app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
         }
     })
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+});
